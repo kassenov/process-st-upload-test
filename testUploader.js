243a20342b75d9c8decc7f83907bbf1ffe6e7846
Zharkyn Kassenov <kzh.light@gmail.com>
2017-01-02 10:37:38 +0000

    progress bar added

--- testUploader.js
+++ testUploader.js
@@ -14,15 +14,15 @@ function TestUploaderController($scope, $element, $attrs) {
     formData: {
       api_password: '8697b84951e481b9c121123f43e8dec1e375049d0e262239462cfcf3b864aa48',
       project_id: 'zh6toltsrl'
-    }
+    },
+    progress: function (e, data) {
+      ctrl.progress = parseInt(data.loaded / data.total * 100, 10);
+    },
   };
 }
 
 angular.module('testApp').component('testUploader', {
   templateUrl: 'testUploader.html',
-  controller: TestUploaderController,
-  bindings: {
-    hero: '='
-  }
+  controller: TestUploaderController
 });
 })(window.angular);
