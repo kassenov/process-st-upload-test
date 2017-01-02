66bf64a54262519d2cc0b36618e95411f8776022
Zharkyn Kassenov <kzh.light@gmail.com>
2017-01-02 11:22:29 +0000

    show video, error mesage and layout update

--- testUploader.js
+++ testUploader.js
@@ -1,6 +1,9 @@
 (function(angular) {
   'use strict';
-function TestUploaderController($scope, $element, $attrs) {
+  
+TestUploaderController.$inject = ['$scope', '$timeout'];
+
+function TestUploaderController($scope, $timeout) {
   var ctrl = this;
   ctrl.options = {
     maxFileSize: 5000000,
@@ -15,9 +18,24 @@ function TestUploaderController($scope, $element, $attrs) {
       api_password: '8697b84951e481b9c121123f43e8dec1e375049d0e262239462cfcf3b864aa48',
       project_id: 'zh6toltsrl'
     },
+    start: function (e) {
+      ctrl.error = null;
+    },
     progress: function (e, data) {
       ctrl.progress = parseInt(data.loaded / data.total * 100, 10);
     },
+    done: function (e, data) {
+      ctrl.mediaHashId = data.result.hashed_id;
+      $timeout(function() {
+          var wistiaEmbed = Wistia.embed(ctrl.mediaHashId, {
+          videoFoam: true,
+          playerColor: "3B97D3"
+        });
+      }, 100);
+    },
+    fail: function (e, data) {
+      ctrl.error = data.errorThrown;
+    }
   };
 }
 
